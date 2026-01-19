d37aa54c8d86ff9f2fb51e3220e0daf91a39cff4
Viveksharma2112 <142593250+Viveksharma2112@users.noreply.github.com>
2026-01-19 09:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (79.77%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
 
-        // dp[i-1] (pichla max)
-        int prev1 = 0; 
-        // dp[i-2] (usse pichla max)
-        int prev2 = 0; 
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-        for (int num : nums) {
-            // Formula: max(aaj ka paisa + i-2 ka profit,  i-1 ka profit)
-            int current = Math.max(prev2 + num, prev1);
-            
-            // Aage badhne ke liye values shift karein
-            prev2 = prev1;
-            prev1 = current;
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
         }
 
-        return prev1;
+        return dp[n - 1];
     }
 }
