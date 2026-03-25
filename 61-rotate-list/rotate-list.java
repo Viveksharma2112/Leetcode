458c7837bba721ae3c37a2742c0f0696beea76f3
Vivek Sharma <142593250+Viveksharma2112@users.noreply.github.com>
2026-03-25 19:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (87.34%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,33 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        // 1. Edge Cases: Agar list khali hai ya rotate nahi karna
-      if(head==null || head.next==null || k==0){
-        return head ;
-      }
 
-      ListNode tail = head ;
-      int len = 1 ; 
+        if(head == null || head.next== null || k==0){
+            return head;
+            
+        }
+        int len = 1 ;
 
-      while(tail.next!=null){
-        tail = tail.next;
-        len++;
-      }
+        ListNode tail = head ; 
+        while(tail.next!=null){
+            tail = tail.next;
+            len++;
+        }
 
-      k = k%len;
-      if(k==0) return head;
-      tail.next = head ; 
+        k = k%len;
 
-      int step = len-k;
+        if(k==0) return head;
+        tail.next = head;
 
-      ListNode temp = tail ; 
-      while(step > 0){
-        temp = temp.next ; 
-        step--;
-      }
 
-      ListNode newhead = temp.next ; 
-      temp.next = null;
+        int step = len-k ;
 
-      return newhead;
+        ListNode temp = tail;
+        
+        while(step>0){
+
+            temp = temp.next;
+            step--;
+        }
+        ListNode curr = temp.next;
+        temp.next = null;
+        return curr;
+    
     }
 }
