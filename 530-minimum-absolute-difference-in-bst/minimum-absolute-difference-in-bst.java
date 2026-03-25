469a82d48715a2882656500c824519fcf3db4287
Vivek Sharma <142593250+Viveksharma2112@users.noreply.github.com>
2026-03-25 19:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (43.73%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.java
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.java
@@ -1,48 +1,41 @@
-        /**
-        * Definition for a binary tree node.
-        * public class TreeNode {
-        *     int val;
-        *     TreeNode left;
-        *     TreeNode right;
-        *     TreeNode() {}
-        *     TreeNode(int val) { this.val = val; }
-        *     TreeNode(int val, TreeNode left, TreeNode right) {
-        *         this.val = val;
-        *         this.left = left;
-        *         this.right = right;
-        *     }
-        * }
-        */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
 
-
-
-        class Solution {
-
-                 int min= Integer.MAX_VALUE;
-
-                 TreeNode prev= null;
-
+    int min = Integer.MAX_VALUE;
+    TreeNode prev = null;
     public int getMinimumDifference(TreeNode root) {
 
-        if( root == null ){
-              return 0;
+        if(root== null){
+            return 0;
         }
 
-           getMinimumDifference(root.left);
+       getMinimumDifference(root.left);
 
-           if( prev!= null){
+       if(prev !=null){
+        min = Math.min(min , Math.abs(root.val - prev.val));
+       }
 
+       prev = root;
 
-                  min = Math.min( min , Math.abs( root.val - prev.val));
+       getMinimumDifference(root.right);
 
-        }
-
-            prev= root; 
-
-                getMinimumDifference(root.right);
-
+       return min;
 
 
-            return min;
-        }
-        } 
+        
+    }
+}
