6e35e9b20687b9fc2a500d9123b2fdc729d10c5a
Vivek Sharma <142593250+Viveksharma2112@users.noreply.github.com>
2026-06-17 17:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (30.46%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] a = {-1,-1};
-
-        a[0] = firstocc(nums, target , 0);
-        a[1] = last(nums, target , 0);
-
-        return a;
-        
+        int[] ans = {-1, -1};
+        ans[0] = findFirst(nums, target);
+        ans[1] = findLast(nums, target);
+        return ans;
     }
 
-    public int firstocc(int[] nums , int target , int i){
-
-        if(i == nums.length){
-            return -1;
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                res = mid;        
+                right = mid - 1; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-
-        if(nums[i]== target){
-            return i ;
-        }
-
-        return firstocc(nums , target ,i+1);
+        return res;
     }
 
-    public int last(int[] nums , int target , int i ){
-        if(i == nums.length ){
-            return -1;
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                res = mid;       
+                left = mid + 1;   // search right side
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-
-        int ans = last(nums, target, i+1);
-
-        if(ans != -1){
-            return ans;
-        }
-
-        if(nums[i] == target){
-            return i ;
-        }
-        return -1;
+        return res;
     }
 }
